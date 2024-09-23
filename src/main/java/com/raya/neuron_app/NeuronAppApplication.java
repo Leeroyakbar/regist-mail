1d0f29369977cd464c06c51e72117d7b2d84472d
lee roy akbar <lee.akbar@jatis.com>
2024-09-23 14:57:00 +0700

    init

---
+++ src/main/java/com/raya/neuron_app/NeuronAppApplication.java
@@ -0,0 +1,13 @@
+package com.raya.neuron_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuronAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raya/neuron_app/NeuronAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raya.neuron_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuronAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
